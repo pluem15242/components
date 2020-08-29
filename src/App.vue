8ad177b1dec516ec24b5ad420f0a2539b69aa07d
pluem15242 <68532575+pluem15242@users.noreply.github.com>
2020-08-30 01:00:46 +0700

    compo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="โหวตเลือกที่ชอบได้เลย" />
+    <hr />
+    <div class="row">
+      <div class="col-6">
+        <img alt="Vue logo" height="400" width="500" src="https://i.pinimg.com/236x/6b/ee/fc/6beefcb0b34bb22f2314f2ca993e3c6d.jpg" />
+        <h1>{{total1}}</h1>
+      </div>
+      <div class="col-6">
+        <img alt="Vue logo" height="400" width="500" src="https://i.pinimg.com/564x/9e/77/07/9e77078acbc2267e77a53292cc7aaeb3.jpg" />
+        <h1>{{total2}}</h1>
+      </div>
+      <div class="col-6">
+        <CSButton @cc="sum" />
+      </div>
+      <div class="col-6">
+        <CSButton @cc="sum1" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CSButton from "./components/CsButton";
+import HelloWorld from "./components/HelloWorld";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CSButton,
+  },
+  data: function () {
+    return {
+      total1: 0,
+      total2: 0,
+    };
+  },
+  methods: {
+    sum(counterValue) {
+      this.total1 = counterValue;
+    },
+    sum1(counterValue) {
+      this.total2 = counterValue;
+    },
+  },
+};
 </script>
 
 <style>
